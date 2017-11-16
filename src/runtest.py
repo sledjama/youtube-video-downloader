80ec1841a6b7ca0ca7f942b11fd82ca57ca40f90
Emmanuel Ajayi <emmanuelajayi@quickenloans.com>
2017-11-16 11:07:38 -0500

    testing appveyor failure

--- src/runtest.py
+++ src/runtest.py
@@ -1,7 +1,7 @@
 import unittest, sys
 import tests.all_tests
 testSuite = tests.all_tests.create_test_suite()
-print("###############")
 text_runner = unittest.TextTestRunner().run(testSuite)
-print(text_runner)
-sys.exit(-1)
+
+if not text_runner.wasSuccessful():
+    sys.exit(-1)
