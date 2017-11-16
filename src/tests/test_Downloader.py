0e27f351a7d422ecac629c6fb36afb4e3ca4f558
Emmanuel Ajayi <emmanuelajayi@quickenloans.com>
2017-11-15 22:33:46 -0500

    Removed old versions, added commands to appveyor

--- src/tests/test_Downloader.py
+++ src/tests/test_Downloader.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from PyQt4 import QtGui, QtCore, QtTest
 
 from functions import *
-from src.Downloader import *
+from Downloader import *
 from _version import __version__
 import configs, os
 
